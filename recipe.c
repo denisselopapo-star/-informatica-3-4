6a5b2cb18d2574ab25087e6e0b493f0e7de8c2c7
denisselopapo-star <denisse.lopapo@gmail.com>
2026-04-20 18:19:32 +0000

    recipe

---
+++ recipe.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("Coconut Cranberry Dessert\n");
+    printf("\n");
+    printf("1 1/2 cups graham cracker crumbs\n");
+    printf("1/2 cup butter or margarine, melted\n");
+    printf("1 can sweetened condensed milk\n");
+}
