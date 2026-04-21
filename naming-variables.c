58f80fd1939a4c25407835b4b3a526889b5916ea
denisselopapo-star <denisse.lopapo@gmail.com>
2026-04-21 21:51:48 +0000

    variables

---
+++ naming-variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+int number; //I fixed the variable changeing the exclamation mark
+int testvalue;// I fixed the variable by eliminating the space
+int example1;// I changed the number to the other side
+float int_count; // I added count
+printf("Variables created successfully. \n");
+}
