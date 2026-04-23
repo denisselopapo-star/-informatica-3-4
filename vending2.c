a633f2cca3cdfd90e3581c871afb2e1f724e008a
denisselopapo-star <denisse.lopapo@gmail.com>
2026-04-23 15:49:05 +0000

    vending2

---
+++ vending2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+     //create variables for first snak (tacos order)
+    float price1 = 135.99;
+    int Quantity_available1 = 15;
+    char Selection_code1 = 'A';
+     //create variables for first snak (tacos order)
+    float price1 = 135.99;
+    int Quantity_available1 = 15;
+    char Selection_code1 = 'A';
+     //create variables for first snak (tacos order)
+    float price1 = 135.99;
+    int Quantity_available1 = 15;
+    char Selection_code1 = 'A';
+
+    printf("Welcome to the Snack Shop!\n");
+    printf("----------------------------------\n");
+    printf("Item\t\tPrice\tQuantity\tCode \n");
+    printf("Price: %.2f price1\t\tPrice\tQuantity available: %d,Quantity_available1\t,Selection code: %c,Selection_code1 \n");
+
+}
