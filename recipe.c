a44ba2418fcf3be5b71e7af77dd86c7adf53a983
denisselopapo-star <denisse.lopapo@gmail.com>
2026-04-23 15:08:07 +0000

    recipe

--- recipe.c
+++ recipe.c
@@ -10,6 +10,6 @@ int main(void)
     printf("1 cup dried cranberries\n");
     printf("2 cups vanilla chips\n");
     printf("1 cup sliced almonds or pecans\n");
-    printf("Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ (33 x 23-cm) pan.\n")
-    printf("Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n")
+    printf("Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ (33 x 23-cm) pan.\n");
+    printf("Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n");
 }
---
+++ vending.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+    //create variables for first snak (tacos order)
+    float price1 = 135.99;
+    int Quantity available = 15;
+    char Selection code = 'A';
+     //create variables for first snak (chips)
+    float price1 = 15.99;
+    int Quantity available = 25;
+    char Selection code = 'B';
+     //create variables for first snak (brownies)
+    float price1 = 19.98;
+    int Quantity available = 10;
+    char Selection code = 'C';
+
+    printf("Vending Machine Stock \n");
+    printf(" \n");
+}
