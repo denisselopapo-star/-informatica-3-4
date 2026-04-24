4fef3601fbe62f02426a4b72e7f843041ec7551e
denisselopapo-star <denisse.lopapo@gmail.com>
2026-04-24 17:23:47 +0000

    calculator

--- rectangle.c
+++ rectangle.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-    
+    int height;
+    int base;
+
+    printf("what is the rectangle height: ");
+    scanf("%d", &height);
+    printf("Whats your rectangle base: ");
+    scanf("%d", &base);
+
+    int area = height*base;
+    int perimeter = 2*(height*base);
+    printf("area, %d \n", area);
+    printf("perimeter, %d \n", perimeter);
 }
